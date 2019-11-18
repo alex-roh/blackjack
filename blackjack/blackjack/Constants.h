7dd4d2fab5525808db98e3bd0d17bc668e41f718
Eunyoung Roh <roheunyoung@outlook.com>
2019-11-18 19:34:02 +0900

    빠트린 Constants.h 파일 추가

---
+++ blackjack/blackjack/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define N_SET 5
+#define N_CARDS_PER_SET 52
+#define N_TOTAL_CARDS 260
+#define N_TYPE 4
+#define N_CARDS_PER_TYPE 13
+#define N_BUFFER 21
+#define N_MAXPLAYERS 5
+#define MAXLENGTH 100
+#define MINLENGTH 30
