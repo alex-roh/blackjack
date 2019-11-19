a1d6b1fb99d87400c0e169373fcaf4d023f7b3c2
eunyoungroh <roheunyoung@outlook.com>
2019-11-19 13:21:28 +0900

    한 라운드 게임 완성
    라운드가 끝난 후 플레이어 구조체 변수 초기화 및 게임 종료조건 검사, 루프 작성만 남음.

--- blackjack/blackjack/game_logic.c
+++ blackjack/blackjack/game_logic.c
@@ -36,7 +36,7 @@ void game_logic_per_round()
 
 	// 유저의 턴 및 다른 플레이어들의 턴
 	int result = 0;
-	printf("\n---------게임 시작--------\n\n");
+	printf("\n---------게임 시작--------\n");
 	
 	for (int i = 0; i < player_number; i++)
 	{
@@ -56,12 +56,29 @@ void game_logic_per_round()
 	}
 		
 	// 이번 라운드 결과 정산
+	printf("\n--------------------------\n");
+	printf("-------라운드 %d 결과------\n", round);
+	printf("--------------------------\n");
+
 	end_round(result, players_bet);
+
+	for (int i = 0; i < player_number; i++)
+	{
+		printf("  --> %s의 결과 : ", players[i].name);
+		if (players[i].is_alive == 1)
+		{
+			printf("승리 ");
+		}
+		else {
+			printf("패배 ");
+		}
+		printf("(총합: %d)  -> 남은 금액: %d\n", players[i].sum, players[i].dollars);
+	}
 	
 	round++;
 }
 
-// 플레이어들의 go/stop 선택
+// 플레이어들의 go/stop 선택 시마다 블랙잭을 판단하는 함수
 void select_go_stop(int i, int* result)
 {
 	if (i == USER) // 유저의 경우
@@ -79,6 +96,7 @@ void select_go_stop(int i, int* result)
 	}
 }
 
+// 플레이어들의 구체적인 go/stop 선택을 정의한 함수
 int selection(player* player_ptr, int i)
 {
 	int stop_flag = 0;
@@ -98,7 +116,7 @@ int selection(player* player_ptr, int i)
 		if (is_blackjack == DEAD)
 		{
 			printf("   -> 총합이 21을 넘었으므로 졌습니다.\n");
-			player_ptr->is_alive = 0;
+			player_ptr->is_alive = 0; // 생존 여부를 0으로 세팅
 			stop_flag = 1;
 			break;
 		}
@@ -129,13 +147,13 @@ int selection(player* player_ptr, int i)
 			{
 				// stop
 				stop_flag = 1;
-				printf("  --> 턴을 다음 플레이어에게 넘깁니다.\n");
+				printf("  --> 턴을 마칩니다.\n");
 			}
 			// 총합이 17 미만이면 go
 			else
 			{
 				// 트레이로부터 새로 카드 한 장을 받아옴
-				if (player_ptr == &dealer) // 서버의 경우
+				if (player_ptr == &dealer) // 딜러의 경우
 				{
 					distribute_cards(DEALER, DEALER);
 				}
@@ -218,10 +236,8 @@ void distribute_cards(int dealer_turn_flag, int i)
 	}
 
 	// 다른 플레이어에게 카드 1장을 분배
-	if (players[i].is_alive == 1)
-	{
-		*(players[i].buffer + players[i].buffer_cursor++) = *(cards_tray + distribution_cursor++);
-	}
+	*(players[i].buffer + players[i].buffer_cursor++) = *(cards_tray + distribution_cursor++);
+
 }
 
 // 다른 플레이어들의 베팅
@@ -237,7 +253,6 @@ void set_players_bet(int* players_bet)
 		{
 			printf("\n  --> %s의 베팅 금액:      %d (보유 금액: %d)\n",
 				players[i].name, *(players_bet + i), players[i].dollars - *(players_bet + i));
-			players[i].dollars -= *(players_bet + i);
 			continue;
 		}
 
@@ -252,13 +267,10 @@ void set_players_bet(int* players_bet)
 
 		printf("  --> %s의 베팅 금액: %d (보유 금액: %d)\n",
 			players[i].name, *(players_bet + i), players[i].dollars - *(players_bet + i));
-		
-		// 베팅액만큼 금액을 상실
-		players[i].dollars -= *(players_bet + i);
-
 	}
 }
 
+// 라운드를 끝내고 결과를 정산함
 void end_round(int is_blackjack, int* players_bet)
 {
 	// 플레이어 중 한 명이 블랙잭이 된 경우
@@ -269,16 +281,63 @@ void end_round(int is_blackjack, int* players_bet)
 
 		if (winner_num == DEALER)  // 딜러가 블랙잭인 경우
 		{
-
+			printf("\n --> 딜러가 블랙잭입니다!\n");
+			// 모든 플레이어들이 패배하고, 자신이 베팅한 금액만큼을 잃음
+			for (int i = 0; i < player_number; i++)
+			{
+				players[i].dollars -= *(players_bet + i);
+				players[i].is_alive = 0;
+			}
 		}
-		else { // 플레이어가 블랙잭인 경우
+		else { // 플레이어들 중 한 명이 블랙잭인 경우
 
+			int bet_total = 0;
+
+			printf("\n --> 블랙잭이 나왔습니다! 블랙잭은 %s입니다.\n\n", players[winner_num].name);
+
+			// 베팅된 금액의 합을 계산
+			for (int i = 0; i < player_number; i++)
+			{
+				bet_total += *(players_bet + i);
+				if (i != winner_num)
+				{
+					// 승리자 이외의 모든 사람들은 자신이 베팅한 금액만큼을 잃음
+					players[i].dollars -= *(players_bet + i);
+					players[i].is_alive = 0;
+				}
+			}
+			
+			// 베팅된 금액 모두를 승리자에게 전달
+			players[winner_num].dollars += bet_total;
 		}
 
 	}
 	// 일반적인 종료 상황의 경우
 	else {
 
+		printf("\n  -> 딜러의 총합: %d\n\n", dealer.sum);
+
+		for (int i = 0; i < player_number; i++)
+		{
+			// 총합이 21을 넘어간 경우 무조건 패배
+			if (players[i].is_alive == 0)
+			{
+				// 베팅한 금액을 잃음
+				players[i].dollars -= *(players_bet + i);
+			}
+			// 총합이 21을 넘지 않으면서 딜러보다 총합이 같거나 큰 경우 승리
+			else if (players[i].is_alive == 1 && players[i].sum >= dealer.sum)
+			{
+				// 베팅한 금액을 얻음
+				players[i].dollars += *(players_bet + i);
+			}
+			// 총합이 21을 넘지 않으면서 딜러가 21을 넘은 경우 승리
+			else if (players[i].is_alive == 1 && dealer.sum > 21)
+			{
+				// 베팅한 금액을 얻음
+				players[i].dollars += *(players_bet + i);
+			}
+		}
 	}
 }
 
